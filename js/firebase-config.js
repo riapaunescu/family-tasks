2c728d0c2599bd4473903458e3e4d89ad6d302ef
Ria Paunescu <paunescu.adriana@yahoo.com>
2026-02-15 19:50:57 +0200

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase Configuration
+// Replace the values below with your Firebase project settings.
+// See SETUP.md for instructions on how to get these values.
+
+const firebaseConfig = {
+apiKey: "AIzaSyBHuFxPnxCSue6vhtjNHGeEkAFl_wVVBGA",
+authDomain: "dbtaskurifamilie.firebaseapp.com",
+databaseURL: "https://dbtaskurifamilie-default-rtdb.europe-west1.firebasedatabase.app",
+projectId: "dbtaskurifamilie",
+storageBucket: "dbtaskurifamilie.firebasestorage.app",
+messagingSenderId: "429075089556",
+appId: "1:429075089556:web:6299beb68ed1d9651d8c81",
+measurementId: "G-1J0GG1H2EB"
+};
